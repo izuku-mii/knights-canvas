87235a6ddfe230ab6bf61fd3b54a833bedefb772
Izuku-mi <151708571+izuku-mii@users.noreply.github.com>
2025-11-13 16:43:25 +0700

    Update Welcome2.js

--- src/greetings2/Welcome2.js
+++ src/greetings2/Welcome2.js
@@ -1,4 +1,5 @@
-const Canvas = require("canvas");
+const { createCanvas, loadImage } = require("canvas");
+const axios = require("axios");
 
 module.exports = class Welcome2 {
 
@@ -10,79 +11,72 @@ module.exports = class Welcome2 {
         this.grupname = "SQUAD-404";
         this.member = "404";
     }
-     //avatar
-    setAvatar(value) {
-        this.avatar = value;
-        return this;
+
+    // setter methods
+    setAvatar(v) { this.avatar = v; return this; }
+    setUsername(v) { this.username = v; return this; }
+    setBg(v) { this.bg = v; return this; }
+    setGroupname(v) { this.grupname = v; return this; }
+    setMember(v) { this.member = v; return this; }
+
+    // helper: load image from URL or local
+    async #load(src) {
+        if (!src) throw new Error("No image source provided");
+        if (/^https?:\/\//.test(src)) {
+            const res = await axios.get(src, { responseType: "arraybuffer" });
+            return await loadImage(Buffer.from(res.data));
+        } else {
+            return await loadImage(src);
+        }
     }
-    //username
-    setUsername(value) {
-        this.username = value;
-        return this;
-    }
-    //bg
-    setBg(value) {
-        this.bg = value;
-        return this;
-    }
-    setGroupname(value) {
-        this.grupname = value;
-        return this;
-    }
-    //member
-    setMember(value) {
-        this.member = value;
-        return this;
-    }
+
     async toAttachment() {
-    
-    
-        // Create canvas
-        const canvas = Canvas.createCanvas(600, 300);
+        const canvas = createCanvas(600, 300);
         const ctx = canvas.getContext("2d");
-       
-   
-        // Draw background
-        let background = await Canvas.loadImage(this.bg);
+
+        // background
+        const background = await this.#load(this.bg);
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-        let fram = await Canvas.loadImage(this.fm);
+
+        // frame
+        const fram = await this.#load(this.fm);
         ctx.drawImage(fram, 0, 0, canvas.width, canvas.height);
 
-        //circle avatar
-    ctx.save();
-	ctx.beginPath();
-	ctx.rotate(-17 * Math.PI / 180);
-	let avatar = await Canvas.loadImage(this.avatar);
-    ctx.strokeStyle = 'white';  // some color/style
-	ctx.lineWidth = 3;  
-	ctx.drawImage(avatar, -4, 130, 113, 113);
-    ctx.strokeRect(-4, 130, 113, 113);
-	ctx.restore();
-    
-        let usrname = this.grupname;
-        let name = usrname.length > 10 ? usrname.substring(0, 10) + "..." : usrname;
+        // avatar (rotated)
+        ctx.save();
+        ctx.beginPath();
+        ctx.rotate(-17 * Math.PI / 180);
+        const avatar = await this.#load(this.avatar);
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 3;
+        ctx.drawImage(avatar, -4, 130, 113, 113);
+        ctx.strokeRect(-4, 130, 113, 113);
+        ctx.restore();
+
+        // group name
+        const usrname = this.grupname;
+        const name = usrname.length > 10 ? usrname.substring(0, 10) + "..." : usrname;
         ctx.globalAlpha = 1;
         ctx.font = "20px CubestMedium";
-        ctx.textAlign = 'center';
+        ctx.textAlign = "center";
         ctx.fillStyle = "#ffffff";
         ctx.fillText(name, 392, 173);
-        
-        
+
+        // member count
         ctx.font = "700 20px Courier New";
-        ctx.textAlign = 'left';
+        ctx.textAlign = "left";
         ctx.fillStyle = "#ffffff";
         ctx.fillText(`${this.member}th member`, 250, 290);
-        
-        let username = this.username;
-        let namalu = username.length > 12 ? username.substring(0, 15) + "..." : username;
+
+        // username
+        const username = this.username;
+        const namalu = username.length > 12 ? username.substring(0, 15) + "..." : username;
         ctx.globalAlpha = 1;
         ctx.font = "700 27px Courier New";
-        ctx.textAlign = 'left';
+        ctx.textAlign = "left";
         ctx.fillStyle = "#ffffff";
         ctx.fillText(namalu, 242, 248);
-         
-	   
+
         return canvas;
     }
-};
-
+}
